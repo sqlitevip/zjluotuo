1e808857f936908fbbc15491600d856206f3fe46
sqlitevip <sqlite.vip@gmail.com>
2024-06-04 15:36:27 +0800

    1

---
+++ datalist.js
@@ -0,0 +1,24 @@
+// name: 车型名称
+// price: 价格
+// path: 图片路径、比如 2024053001
+// imgnum: 图片张数、jpg格式、比如1.jpg
+var datalist = [
+	{name:'2017款宝马5系525Li 领先型',price:'14.x万',path:'2024051401',imgnum:9},
+	{name:'2017款宝马5系525Li 领先型2',price:'14.x万',path:'2024051401',imgnum:9},
+	{name:'2017款宝马5系525Li 领先型3',price:'14.x万',path:'2024051401',imgnum:9},
+	{name:'2017款宝马5系525Li 领先型4',price:'14.x万',path:'2024051401',imgnum:9},
+	{name:'2017款宝马5系525Li 领先型5',price:'14.x万',path:'2024051401',imgnum:9},
+	{name:'2017款宝马5系525Li 领先型6',price:'14.x万',path:'2024051401',imgnum:9},
+	{name:'2017款宝马5系525Li 领先型7',price:'14.x万',path:'2024051401',imgnum:9},
+	{name:'2017款宝马5系525Li 领先型8',price:'14.x万',path:'2024051401',imgnum:9},
+	{name:'2017款宝马5系525Li 领先型9',price:'14.x万',path:'2024051401',imgnum:9},
+	{name:'2017款宝马5系525Li 领先型10',price:'14.x万',path:'2024051401',imgnum:9},
+	{name:'2017款宝马5系525Li 领先型11',price:'14.x万',path:'2024051401',imgnum:9},
+	{name:'2017款宝马5系525Li 领先型12',price:'14.x万',path:'2024051401',imgnum:9},
+	{name:'2017款宝马5系525Li 领先型13',price:'14.x万',path:'2024051401',imgnum:9},
+	{name:'2017款宝马5系525Li 领先型14',price:'14.x万',path:'2024051401',imgnum:9},
+	{name:'2017款宝马5系525Li 领先型15',price:'14.x万',path:'2024051401',imgnum:9},
+	{name:'2017款宝马5系525Li 领先型16',price:'14.x万',path:'2024051401',imgnum:9},
+	{name:'2017款宝马5系525Li 领先型17',price:'14.x万',path:'2024051401',imgnum:9},
+	{name:'2017款宝马5系525Li 领先型18',price:'14.x万',path:'2024051401',imgnum:9},
+];
